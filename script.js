95082f4e38b81b0cb39230c1920b48cb85e764a8
maxihate <torihate555@gmail.com>
2019-10-11 01:43:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,10 @@ let startDate = new Date(2019,9,1);
 let nowDate = Date.now();
 let leftTime = Math.floor((targetDate - nowDate) / 1000);
 
-counter.innerHTML = `${Math.floor((leftTime / 3600) / 24)} dni
-                <br>${Math.floor(leftTime / 3600) % 24} godziny
-                <br>${Math.floor(leftTime / 60) % 60} minuty
-                <br>${leftTime % 60} sekundy
-`;
+counter.innerHTML = Math.floor((leftTime / 3600) / 24) + " dni"
++"<br>" + Math.floor(leftTime / 3600) % 24 + " godziny"
++"<br>" + Math.floor(leftTime / 60) % 60 + " minuty"
++"<br>" + leftTime % 60 + " sekundy";
 
 }
 
