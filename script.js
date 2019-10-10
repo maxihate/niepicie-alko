a1463faa7dd1318d8efa8d7a048248ad9bd72c97
maxihate <torihate555@gmail.com>
2019-10-11 01:34:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const counter = document.querySelector('.countdown-right');
+
+
+function cpn() {
+
+let targetDate = new Date(2020,0,30);
+let startDate = new Date(2019,9,1);
+let nowDate = Date.now();
+let leftTime = Math.floor((targetDate - nowDate) / 1000);
+
+counter.innerHTML = `${Math.floor((leftTime / 3600) / 24)} dni
+                <br>${Math.floor(leftTime / 3600) % 24} godziny
+                <br>${Math.floor(leftTime / 60) % 60} minuty
+                <br>${leftTime % 60} sekundy
+`;
+
+}
+
+function pogonSzczecin() {
+
+cpn();
+
+}
+
+cpn();
+setInterval(pogonSzczecin, 1000);
