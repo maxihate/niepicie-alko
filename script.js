c5e696cdd4a1937497a158b8d9dbb51074184a5c
maxihate <torihate555@gmail.com>
2019-10-11 13:06:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const counter = document.getElementsByClassName('countdown-right')[0];
+const counter = document.querySelector('.countdown-right');
 
 
 function cpn() {
@@ -8,16 +8,42 @@ let startDate = new Date(2019,9,1);
 let nowDate = Date.now();
 let leftTime = Math.floor((targetDate - nowDate) / 1000);
 
-counter.innerHTML = Math.floor((leftTime / 3600) / 24) + " dni"
-+"<br>" + Math.floor(leftTime / 3600) % 24 + " godziny"
-+"<br>" + Math.floor(leftTime / 60) % 60 + " minuty"
-+"<br>" + leftTime % 60 + " sekundy";
+let time = {
+  "days": Math.floor((leftTime / 3600) / 24),
+  "minutes": Math.floor(leftTime / 60) % 60,
+  "hours": Math.floor(leftTime / 3600) % 24,
+  "seconds": leftTime % 60
+};
 
+function threeSixMafia(num, type) {
+
+    let numArr = num.toString().split("");
+    let numLast = numArr[numArr.length-1];
+    let numRegExp = new RegExp("(2|3|4)");
+    let numMatch = (numLast.match(numRegExp)) ? (num > 14 || num < 11 ? true : false) : false;
+
+    switch (type) {
+      case "hours":
+        return (numMatch ? "godziny" : (num === 1 ? "godzina" : "godzin"));
+      case "minutes":
+        return (numMatch ? "minuty" : (num === 1 ? "minuta" : "minut"));
+      case "seconds":
+        return (numMatch ? "sekundy" : (num === 1 ? "sekunde" : "sekund"));
+      default:
+        return undefined;
+    }
+
+}
+
+counter.innerHTML = time.days + (time.days === 1 ? " dzien" : " dni")
+  +"<br>" + time.hours + " " + threeSixMafia(time.hours, "hours")
+  +"<br>" + time.minutes + " " + threeSixMafia(time.minutes, "minutes")
+  +"<br>" + time.seconds  + " " + threeSixMafia(time.seconds, "seconds");
 }
 
 function pogonSzczecin() {
 
-cpn();
+  cpn();
 
 }
 
